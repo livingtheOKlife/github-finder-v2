52e151cfe3c678f4bcc0cc7aecc18a2ea3147054
Oliver Kelly <oliver.philip.kelly@outlook.com>
2024-04-13 15:51:10 +0100

    creates loading element and applies relevant styles

---
+++ src/components/shared/Loading.jsx
@@ -0,0 +1,37 @@
+/**--------------------------------------------------------------------------- Loading component function
+ * 
+ * @name Loading
+ * 
+ * @function
+ * 
+ * @description loading === true
+ * 
+ * @returns div.loading
+ * 
+ * @author livingtheOKlife
+ * 
+ * @date 20.03.2024
+ * 
+ * -------------------------------------------------- */
+
+const Loading = () => {
+
+  // ------------------------- return
+
+  return (
+
+    <div className='loading'>
+
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+
+    </div>
+
+  )
+
+}
+
+// --------------------------------------------------------------------------- Loading component export
+
+export default Loading
